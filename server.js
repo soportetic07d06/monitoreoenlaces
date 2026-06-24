de71cff04266d245bb9cf766ffebb3500497799a
soportetic07d06 <soportetic.07d06@gmail.com>
2026-06-24 00:02:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const net = require('net');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
